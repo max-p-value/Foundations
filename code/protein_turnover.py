d869b5426215ef0fda8aa97fc2a20cb356792d28
pwahle <philipp.wahle@mdc-berlin.de>
2025-06-05 17:25:17 +0200

    tralala

---
+++ code/protein_turnover.py
@@ -0,0 +1,21 @@
+class ProteinTurnover:
+    def __init__(self, transcription_rate, translation_rate, degradation_rate, dt=1.0):
+        self.mRNA = 0.0
+        self.protein = 0.0
+        self.transcription_rate = transcription_rate
+        self.translation_rate = translation_rate
+        self.degradation_rate = degradation_rate
+        self.dt = dt
+        self.time = 0.0
+        self.history = []
+
+    def step(self):
+        self.mRNA += self.transcription_rate * self.dt
+        self.protein += self.translation_rate * self.mRNA * self.dt
+        self.protein -= self.degradation_rate * self.protein * self.dt
+        self.time += self.dt
+        self.history.append((self.time, self.mRNA, self.protein))
+
+    def run(self, steps):
+        for _ in range(steps):
+            self.step()
