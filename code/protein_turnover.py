4a5da428761b90b03aff8ed8731186b1a742295c
pwahle <philipp.wahle@mdc-berlin.de>
2025-06-06 05:16:07 +0200

    introduced mRNA degradation rate

--- code/protein_turnover.py
+++ code/protein_turnover.py
@@ -1,16 +1,18 @@
 class ProteinTurnover:
-    def __init__(self, transcription_rate, translation_rate, degradation_rate, dt=1.0):
+    def __init__(self, transcription_rate, translation_rate, degradation_rate, mRNA_degradation_rate, dt=1.0):
         self.mRNA = 0.0
         self.protein = 0.0
         self.transcription_rate = transcription_rate
         self.translation_rate = translation_rate
         self.degradation_rate = degradation_rate
+        self.mRNA_degradation_rate = mRNA_degradation_rate
         self.dt = dt
         self.time = 0.0
         self.history = []
 
     def step(self):
         self.mRNA += self.transcription_rate * self.dt
+        self.mRNA -= self.mRNA_degradation_rate * self.mRNA * self.dt
         self.protein += self.translation_rate * self.mRNA * self.dt
         self.protein -= self.degradation_rate * self.protein * self.dt
         self.time += self.dt
